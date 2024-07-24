ce46fad7ce378bb8df21730588de04c0412d8108
dan koenig <dangerbird@dans-MacBook-Pro.local>
2024-07-23 20:59:03 -0400

    extend user functionality

--- main.py
+++ main.py
@@ -9,10 +9,44 @@ class GameManager():
     INSTANCE = None
     def __init__(self):
         self.game = RummyGame(player_list=["computer", "human"])
+        self.stage = None
         GameManager.INSTANCE = self
     
     def execute_turn(self, data=None):
         self.game.take_turn(lambda: print('Turn Taken'))
+    
+    def open_discard_modal(self, parent, should_draw_card=True):
+        if should_draw_card:
+            GameManager.INSTANCE.game.draw_card(GameManager.INSTANCE.game.get_current_player())
+        modal = DiscardModal(parent)
+        modal.open_modal()
+    
+    def open_match_maker_modal(self, parent):
+        modal = MatchMakerModal(parent)
+        modal.open_modal()
+    
+    def create_modal(self, parent, buttons=[], label=None):
+        modal = BaseModal(parent)
+        label_widget = None
+        if label:
+            label_widget = modal.add_label(text=label['text'], container=modal)
+        button_widgets = []
+        if buttons:
+            button_layout = BoxLayout()
+            modal.add_widget(button_layout)
+            for x in buttons:
+                modal.add_button(text=x['text'], action=x['action'], container=button_layout)
+            # button_widgets = map(lambda x: modal.add_button(text=x['text'], action=x['action'], container=modal), buttons)
+        
+        modal.open_modal()
+
+        return { 'buttons': button_widgets, 'label': label_widget }
+    
+    def get_options(self):
+        return  self.game.get_set_options(include_hand=True, include_pile=True)
+        
+    def get_opps(self):
+        return self.game.get_existing_set_opportunitie(include_pile=True)
     
 class CardLayout(BoxLayout):
     def __init__(self, card, card_color=(2.5,2.5,2.5,1), **kwargs):
@@ -151,13 +185,13 @@ class BaseModal(BoxLayout):
     OPEN_MODALS = []
     def __init__(self, parent, **kwargs):
         super().__init__(*kwargs)
-        self.parent_widget = parent
+        self.parent_widget = parent if parent else GameManager.INSTANCE.stage
         self.orientation = 'vertical'
+
         self.create_ui()
     
     def create_ui(self):
-        test_label = Label(text='Test Message')
-        self.add_widget(test_label)
+        pass
 
     def open_modal(self):
         self.parent_widget.add_widget(self)
@@ -166,30 +200,59 @@ class BaseModal(BoxLayout):
         BaseModal.OPEN_MODALS = []
         BaseModal.OPEN_MODALS.append(self)
         print(BaseModal.OPEN_MODALS)
+
+    def add_button(self, text="button", action=None, container=None):
+        button = Button(text=text)
+        button.on_release = action
+        if container:
+            container.add_widget(button)
+        else:
+            self.add_widget(button)
+
+        return button
+    
+    def add_label(self, text, container=None):
+        label = Label(text=text)
+        if container:
+            container.add_widget(label)
+        else:
+            self.add_widget(label)
+        
+        return label
+        
     
     def close_modal(self):
         self.parent_widget.remove_widget(self)
+        
     
 class TurnStartModal(BaseModal):
     def create_ui(self):
         top_box = BoxLayout()
 
-        message_label = Label(text='Do you want to select from the discard pile?')
-        top_box.add_widget(message_label)
+        self.add_label(text='Do you want to lay down some cards?', container=top_box)
 
         bottom_box = BoxLayout()
-        yes_button = Button(text='yes')
-        no_button = Button(text='no')
-        no_button.on_release = self.on_no
-        bottom_box.add_widget(yes_button)
-        bottom_box.add_widget(no_button)
+        self.add_button(text="yes", container=bottom_box)
+        self.add_button(text="no", container=bottom_box, action=self.on_no)
 
         self.add_widget(top_box)
         self.add_widget(bottom_box)
     
     def on_no(self, data=None):
-        modal = DiscardModal(self.parent)
-        modal.open_modal()
+        options = GameManager.INSTANCE.get_options()['options']
+        opps = GameManager.INSTANCE.get_opps()['opps']
+        if len(options) or len(opps):
+            buttons = [
+                {'text':'Yes', 'action': self.discard_modal},
+                {'text':'No', 'action':lambda: GameManager.INSTANCE.open_match_maker_modal(self.parent)}
+            ]
+            label = {'text':'Are you sure? There are some cards you can play.'}
+            GameManager.INSTANCE.create_modal(self.parent,buttons=buttons,label=label)
+        else:
+            self.discard_modal()
+    
+    def discard_modal(self):
+        GameManager.INSTANCE.open_discard_modal(self.parent)
 
 
     
@@ -197,8 +260,7 @@ class DiscardModal(BaseModal):
     def create_ui(self):
         top_box = BoxLayout()
 
-        message_label = Label(text='What card do you want to discard')
-        top_box.add_widget(message_label)
+        self.add_label(text='What card do you want to discard', container=top_box)
 
         self.add_widget(top_box)
 
@@ -217,10 +279,56 @@ class DiscardModal(BaseModal):
         # discard selected card
         # end turn
 
+class MatchMakerModal(BaseModal):
+    def create_ui(self):
+        self.current_match = []
+        hand_layout = BoxLayout()
+        player_hand = CardGroupLayout()
+        player_hand.card_click_handler = self.on_card_selected
+        player_hand.set_cards(GameManager.INSTANCE.game.get_current_player()['hand'])
+        hand_layout.add_widget(player_hand)
+
+        discard_layout = BoxLayout()
+        discard_pile = CardGroupLayout()
+        discard_pile.card_click_handler = self.on_card_selected
+        discard_pile.set_cards(GameManager.INSTANCE.game.discard_pile)
+        discard_layout.add_widget(discard_pile)
+
+        match_layout = BoxLayout()
+        self.match_pile = CardGroupLayout()
+        self.match_pile.card_click_handler = self.on_card_deselected
+        match_layout.add_widget(self.match_pile)
+
+        button_layout = BoxLayout()
+        self.add_button(text='Create Match', action=lambda: self.add_match(), container=button_layout)
+        self.add_button(text='Confirm', action=lambda: self.close_modal(), container=button_layout)
+
+        self.add_widget(hand_layout)
+        self.add_widget(discard_layout)
+        self.add_widget(match_layout)
+        self.add_widget(button_layout)
+
+    def on_card_selected(self, card):
+        self.current_match.append(card)
+        self.match_pile.set_cards(self.current_match)
+        print('card to discard')
+        print(card)
+    
+    def on_card_deselected(self, card):
+        print('remove card from match')
+
+    def add_match(self):
+        print('create match')
+    
+
+
+
 class RummyApp(App):
     def build(self):
-        GameManager()
-        return GameLayout()
+        manager = GameManager()
+        game_layout = GameLayout()
+        manager.stage = game_layout
+        return game_layout
 
 if __name__ == "__main__":
     RummyApp().run()
